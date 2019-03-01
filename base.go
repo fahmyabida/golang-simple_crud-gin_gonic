6bd2504c434d917d82b5dba93b4832d6ef1e3ebd
Fahmy Abida <fahmyabida@gmail.com>
2019-03-01 11:14:33 +0700

    grouping router

--- base.go
+++ base.go
@@ -8,12 +8,19 @@ import (
 
 func main() {
 	dbConn := config.DBInit()
-	hero := &controllers.HeroController{Db:dbConn}
+	hero := &controllers.HeroController{Db: dbConn}
 	router := gin.Default()
-	router.GET("/heroes", hero.GetAllHero)
-	router.GET("/v2/heroes", hero.GetAllHeroWithDTO)
-	router.DELETE("/hero", hero.DeleteHero)
-	router.POST("/hero", hero.CreateHeroWithPostForm)
-	router.POST("/v2/hero", hero.CreateHeroWithBodyRequest)
-	router.Run(":8080")
+	version1 := router.Group("/v1")
+	{
+		version1.GET("/heroes", hero.GetAllHero)
+		version1.DELETE("/hero", hero.DeleteHero)
+		version1.POST("/hero", hero.CreateHeroWithPostForm)
+	}
+
+	version2 := router.Group("/v2")
+	{
+		version2.GET("/v2/heroes", hero.GetAllHeroWithDTO)
+		version2.POST("/v2/hero", hero.CreateHeroWithBodyRequest)
+		router.Run(":8080")
+	}
 }
